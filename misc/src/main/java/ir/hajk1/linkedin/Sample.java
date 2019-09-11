470f5dfbb0580c91735248af8846b07118bfcc22
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2019-09-11 14:30:11 +0430

    adding new general module

---
+++ misc/src/main/java/ir/hajk1/linkedin/Sample.java
@@ -0,0 +1,47 @@
+package ir.hajk1.linkedin;
+
+/**
+ * Author: <a href="mailto:k1.tehrani@gmail.com">Kayvan Tehrani</a>
+ *
+ * Description: <the description of the class for java doc by those that might use it, please use html if possible>
+ */
+public class Sample {
+
+  static String s;
+
+  public static void main(String[] args) {
+//    System.out.println(hello());
+//    String s = "strawberries";
+//    System.out.println(s.substring(2,5));
+////    int a = 1234561234512345;
+//    short s1=0,s2 = 0;
+//    byte b1=0,b2 = 0;
+////    short s3 = s1+s2;
+////    byte b3 = b1+b2;
+//    System.out.println(s);
+//    List  list = new ArrayList();
+//    list.add("");
+//    list.add(2);
+//    System.out.println("list.get(0) = " + list.get(0) instanceof Object);
+////    System.out.println("list.get(0) = " + list.get(1) instanceof Integer);
+//    int ar[]= {1,2,3,4};
+//    for (int i = 0; i < 10; i=i++) {
+//      i+=1;
+//      System.out.println("Hiiiiiiiiii");
+//
+//      String[] l1 = {"a","b","c"};
+//      String[] l2 = {"a","b","c"};
+//      String[] l3 = l1;
+//      System.out.println(l1.equals(l2));
+//      System.out.println(l1.equals(l3));
+
+    String s = "Hello World!";
+    System.out.println(s.substring(6, 12) + s.substring(12, 6));
+
+  }
+
+  private static Exception hello() {
+    return new Exception();
+  }
+
+}
