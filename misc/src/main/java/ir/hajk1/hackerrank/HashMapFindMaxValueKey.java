2d638eeee03cf3157aeaae4d5aeaaf31cae59d2e
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2019-10-12 14:15:40 +0330

    adding hashMap find Max value KEY

---
+++ misc/src/main/java/ir/hajk1/hackerrank/HashMapFindMaxValueKey.java
@@ -0,0 +1,29 @@
+package ir.hajk1.hackerrank;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: <a href="mailto:k1.tehrani@gmail.com">Kayvan Tehrani</a>
+ *
+ * Description: <the description of the class for java doc by those that might use it, please use html if possible>
+ */
+public class HashMapFindMaxValueKey {
+
+  private HashMap<Integer, Integer> map = new HashMap<>();
+
+  public static void main(String[] args) {
+    HashMapFindMaxValueKey hashMapMax = new HashMapFindMaxValueKey();
+    hashMapMax.map.put(2, 3);
+    hashMapMax.map.put(1, 0);
+    hashMapMax.map.put(5, 1);
+    System.out.println("hashMapMax = " + hashMapMax.findMaxIndex());
+  }
+
+  private int findMaxIndex() {
+    return map.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue))
+        .map(Map.Entry::getKey).orElse(null);
+  }
+
+}
