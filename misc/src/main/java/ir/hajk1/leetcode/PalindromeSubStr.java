3236f260d696dca76e8e73e875c1bf8702e3f9d8
hajk1 <k1.tehrani@gmail.com>
2023-02-07 14:53:11 +0100

    final changes

--- misc/src/main/java/ir/hajk1/leetcode/PalindromeSubStr.java
+++ misc/src/main/java/ir/hajk1/leetcode/PalindromeSubStr.java
@@ -17,30 +17,27 @@ public class PalindromeSubStr {
     }
 
     public String longestPalindrome(String s) {
-        String result = "";
+        int start = 0, end = 0;
         if (s.length() == 1)
             return s;
         for (int i = 0; i < s.length(); i++) {
             int j = s.length() - 1;
             while (j >= i) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    if ((j - i) > result.length() - 1) {
-                        String s1 = s.substring(i, j + 1);
-                        if (isPalindrome(s1)) {
-                            result = s1;
-                        }
+                if ((j - i) > (end - start) - 1) {
+                    if (isPalindrome(s, i, j)) {
+                        start = i;
+                        end = j;
                     }
                 }
                 j--;
             }
-
         }
-        return result;
+        return s.substring(start, end + 1);
     }
 
-    private boolean isPalindrome(String s1) {
-        for (int i = 0; i < s1.length() / 2; i++) {
-            if (s1.charAt(i) != s1.charAt(s1.length() - i - 1))
+    private boolean isPalindrome(String s1, int startIndex, int endIndex) {
+        for (int i = 0; i < (endIndex - startIndex + 1) / 2; i++) {
+            if (s1.charAt(startIndex + i) != s1.charAt(endIndex - i))
                 return false;
         }
         return true;
