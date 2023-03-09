87f26fe8f4e6349c788f4fbee36989e4530f5500
hajk1 <k1.tehrani@gmail.com>
2023-03-09 18:35:36 +0100

    final version

--- misc/src/main/java/ir/hajk1/hackerrank/NavanChallenge.java
+++ misc/src/main/java/ir/hajk1/hackerrank/NavanChallenge.java
@@ -1,8 +1,10 @@
 package ir.hajk1.hackerrank;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,12 +22,22 @@ public class NavanChallenge {
 
     public static int degreeOfArray(List<Integer> arr) {
         Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Deque<Integer>> dequeMap = new HashMap<>();
+        int j = 0;
         for (Integer i : arr) {
+            Deque<Integer> deq;
             if (map.containsKey(i)) {
                 map.put(i, map.get(i) + 1);
+                deq = dequeMap.get(i);
+                deq.offer(j);
+                dequeMap.put(i, deq);
             } else {
                 map.put(i, 1);
+                deq = new ArrayDeque<>();
+                deq.offer(j);
+                dequeMap.put(i, deq);
             }
+            j++;
         }
 
         List<Integer> keys = new ArrayList<>();
@@ -48,8 +60,13 @@ public class NavanChallenge {
 
         Set<Integer> sizeSet = new TreeSet<>();
         for (Integer i : keys) {
-            List<Integer> list = arr.subList(arr.indexOf(i), arr.lastIndexOf(i) + 1);
+            Deque<Integer> que = dequeMap.get(i);
+            int start = que.peekFirst();
+            int end = que.peekLast();
+
+            List<Integer> list = arr.subList(start, end + 1);
             System.out.println("sub list: " + list);
+
             sizeSet.add(list.size());
         }
         return sizeSet.stream().findFirst().get();
